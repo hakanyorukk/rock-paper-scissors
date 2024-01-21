f9a79b86f4f67f7fb08fc6ad60ac8b820e038072
hakanyorukk <hakanyorukomer@hotmail.com>
2024-01-22 00:05:59 +0200

    Final commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = () => {
   let pScore = 0;
-  let sScore = 0;
+  let cScore = 0;
 
   //Start the game
   const startGame = () => {
@@ -16,8 +16,15 @@ const game = () => {
   //PLay Match
   const playMatch = () => {
     const options = document.querySelectorAll('.options button');
-    const playerHande = document.querySelector('.player-hand');
+    const playerHand = document.querySelector('.player-hand');
     const computerHand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
+
+    hands.forEach(hand => {
+      hand.addEventListener('animationend', function () {
+        this.style.animation = '';
+      });
+    });
 
     //Computer Options
     const computerOptions = ['rock', 'paper', 'scissors'];
@@ -27,9 +34,79 @@ const game = () => {
         //Computer Choice
         const computerNumber = Math.trunc(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
-        console.log(computerChoice);
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+
+          //Update images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+        //Animaiton
+        playerHand.style.animation = 'shakePlayer 2s ease';
+        computerHand.style.animation = 'shakeComputer 2s ease';
       });
     });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update text
+    const winner = document.querySelector('.winner');
+    //Checking for a tie.
+    if (playerChoice === computerChoice) {
+      winner.textContent = 'It is a tie.';
+      updateScore();
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === 'rock') {
+      if (computerChoice === 'scissors') {
+        winner.textContent = 'You won!';
+        pScore++;
+        updateScore();
+        return;
+      }
+    } else {
+      winner.textContent = 'Computer won!';
+      cScore++;
+      updateScore();
+      return;
+    }
+    //Check for paper
+    if (playerChoice === 'paper') {
+      if (computerChoice === 'rock') {
+        winner.textContent = 'You won!';
+        pScore++;
+        updateScore();
+        return;
+      }
+    } else {
+      winner.textContent = 'Computer won!';
+      cScore;
+      updateScore();
+      return;
+    }
+    //Check for scissors
+    if (playerChoice === 'scissors') {
+      if (computerChoice === 'paper') {
+        winner.textContent = 'You won!';
+        pScore++;
+        updateScore();
+        return;
+      }
+    } else {
+      winner.textContent = 'Computer won!';
+      cScore++;
+      updateScore();
+      return;
+    }
   };
 
   //Is call all the inner function
